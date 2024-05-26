3dda04ffa9efbe69ec30ec150db2f6120b5b225d
Alexandr <shura.tabolich@mail.ru>
2024-05-26 20:06:09 +0300

    add headers

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Credentials: true');
+
 
 // Функция для выполнения HTTP-запроса и получения содержимого страницы
 function fetchPageContent($url) {
     $ch = curl_init();
-    
+
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    
+
     $response = curl_exec($ch);
     curl_close($ch);
-    
+
     return $response;
 }
 
